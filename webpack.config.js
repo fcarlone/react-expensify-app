746f55356e6b1d71657d7ae874a8829245c3f890
fcarlone <frankcarlone@gmail.com>
2018-10-18 16:00:08 -0400

    Add Readme file

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,38 @@
 const path = require('path');
 // console.log(path.join(__dirname, 'public'));
 
-module.exports = {
-  mode: 'none',
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /.js$/,
-        exclude: /node_modules/
-      }, {
-        test: /.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
-};
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  console.log('env', env);
+  return {
+    mode: 'none',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /.js$/,
+          exclude: /node_modules/
+        }, {
+          test: /.s?css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
+}
 
