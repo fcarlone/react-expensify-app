fa48f545a6f0dbe67f01aa15c6f25fefe7cacc10
fcarlone <frankcarlone@gmail.com>
2018-11-02 12:03:29 -0400

    Add Public and Private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -10,15 +10,16 @@ import PageNotFound from '../components/PageNotFound';
 import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
+
       <Switch>
-        <Route path="/" component={LoginPage} exact={true} />
+        <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
