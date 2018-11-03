adf011e59b7e75a93ba74dcbba6fa8d7779d99bd
fcarlone <frankcarlone@gmail.com>
2018-11-03 13:40:27 -0400

    Style dashboard page and add expense page

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
@@ -9,15 +10,13 @@ export const ExpenseSummary = (props) => {
   const formattedExpensesTotal = numeral(props.expensesTotal / 100).format('$0,0.00');
 
   return (
-    <div>
-      <p>Expenses Summary</p>
-      {
-        props.expenseCount !== 0 ? (
-          <p>Viewing {props.expenseCount} {expenseWord} totalling {formattedExpensesTotal}</p>
-        ) : (
-            <p></p>
-          )
-      }
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{props.expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+        <div className="page-header__actions">
+          <Link className="button-layout" to="/create">Add Expenses</Link>
+        </div>
+      </div>
     </div>
   )
 };
